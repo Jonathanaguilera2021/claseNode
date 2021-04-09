42f36ed0c21c65fa6c27fd68db730ef6bcacafe9
Jonathanaguilera2021 <jaguilera277@gmail.com>
2021-04-08 20:42:27 -0500

    [update] construccion de API

---
+++ appServer/src/controllers/course.controller.js
@@ -0,0 +1,10 @@
+import Course from "../models/Course"
+
+export const createCourse = (req, res) => {
+    res.send("Creando")
+}
+export const getCourses = () => {} //trae los cursos
+export const getCoursesById = () => {} //Un curso especifico
+export const updateCourse = () => {}
+export const deleteCourse = () => {}
+
